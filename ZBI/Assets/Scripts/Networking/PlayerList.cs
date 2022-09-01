f7766629c36e239d44a2c9df919cf8ad151684db
Ritosalgado <ritosalgado@gmail.com>
2022-09-01 17:59:59 +0200

    Debug Lobby Text

--- ZBI/Assets/Scripts/Networking/PlayerList.cs
+++ ZBI/Assets/Scripts/Networking/PlayerList.cs
@@ -3,37 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 namespace ZBI {
 
     public class PlayerList : MonoBehaviourPunCallbacks
     {
         [SerializeField] private Transform _content;
-        public List<Player> playerList;
+        private List<Player> _playerList = new List<Player>();
+
+        public TextMeshProUGUI debugText;
 
         public void UpdatePlayersList()
         {
             //Instantiate the prefab in the dynamic list
+
         }
 
         #region PUN Callbacks
         public override void OnPlayerEnteredRoom(Player newPlayer)
         {
-            playerList.Add(newPlayer);
+            _playerList.Add(newPlayer);
 
-            foreach (Player player in playerList)
+            foreach (Player player in _playerList)
             {
                 Debug.Log(player);
+                debugText.text = player.NickName + " entered the room!";
             }
         }
 
         public override void OnPlayerLeftRoom(Player otherPlayer)
         {
-            foreach (Player player in playerList)
+            foreach (Player player in _playerList)
             {
                 if (otherPlayer.UserId == player.UserId)
                 {
-                    playerList.Remove(player);
+                    _playerList.Remove(player);
+                    debugText.text = player.NickName + " left the room!";
                 }
             }
         }
